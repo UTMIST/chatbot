c73a6954ef263ad92e4b64cd9a6cdfeb5a09df93
Michael Liao <michaell.liao44452@gmail.com>
2024-06-15 10:04:31 -0400

    changed secret for discordbot.py in app

--- app/discordbot.py
+++ app/discordbot.py
@@ -51,4 +51,4 @@ async def on_reaction_add(reaction, user):
     await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
 
 
-client.run('MTI1MTQxNjQyMTExNjY3ODIwNg.GeD3Qi.fcyerm1k3uVNOe2MN9dDNo5Q0VV_rOmRKc54J8')
+client.run("Your-token")
